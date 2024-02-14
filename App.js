a8908afe4c19dd2211e33f709211a6d4d49bb606
Jacob Kristensen <jacobtkristensen@gmail.com>
2024-02-14 16:43:57 +0100

    Temp work

--- App.js
+++ App.js
@@ -4,20 +4,25 @@ import { Button, StyleSheet, Text, View, TextInput, FlatList } from 'react-nativ
 
 export default function App() {
 
-const [text, setText] = useState('')
+  const [text, setText] = useState('')
 
-const notes = [{key:1}]
+  const [notes, setNotes] = useState([])
 
   function buttonHandler() {
-    alert("you typed " + text)
+    setNotes([...notes, { key: notes.length, name: text }])
+    setText('')
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Notebook <br></br> </Text>
-      <TextInput style={styles.TextInput} onChangeText={(txt) => setText(txt)}></TextInput>
+      <TextInput 
+      style={styles.TextInput} onChangeText={(txt) => setText(txt)} value={text}></TextInput>
       <Button title="Add to list" onPress={buttonHandler}></Button>
-      <FlatList  />
+      <FlatList style={styles.list}
+        data={notes}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -26,19 +31,23 @@ const notes = [{key:1}]
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25282e',
     alignItems: 'center',
     justifyContent: 'center',
     color: '#fff',
   },
-  title:{
+  title: {
     textAlign: 'center',
     fontSize: 20,
+    color: 'lightblue',
   },
   TextInput: {
-    backgroundColor: 'lightblue' ,
+    backgroundColor: 'lightblue',
     minHeight: 30,
     minWidth: 200,
     marginBottom: 10
   },
+  list: {
+    color: 'lightblue',
+  }
 });
