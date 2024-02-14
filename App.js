c09afbd2d55e689bfa3aa8c5cde73a1743d58f7b
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2024-02-14 16:03:17 +0100

    temp

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+
+const [text, setText] = useState('')
+
+const notes = [{key:1}]
+
+  function buttonHandler() {
+    alert("you typed " + text)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Notebook <br></br> </Text>
+      <TextInput style={styles.TextInput} onChangeText={(txt) => setText(txt)}></TextInput>
+      <Button title="Add to list" onPress={buttonHandler}></Button>
+      <FlatList  />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +29,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#fff',
+  },
+  title:{
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  TextInput: {
+    backgroundColor: 'lightblue' ,
+    minHeight: 30,
+    minWidth: 200,
+    marginBottom: 10
   },
 });
