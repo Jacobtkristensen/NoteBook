f4fd463ab187072ae593766db84e42f1b1dc9ec2
Jacob Kristensen <jacobtkristensen@gmail.com>
2024-03-12 17:26:35 +0100

    NoteBook camera complete

--- DetailsPage.js
+++ DetailsPage.js
@@ -2,43 +2,94 @@ import { View, TextInput, Button, StyleSheet, Image } from 'react-native'
 import { useState } from 'react';
 import { doc, updateDoc } from 'firebase/firestore';
 import * as ImagePicker from 'expo-image-picker';
-import { getStorage, storage } from './firebase.js';
+import { database, storage } from './firebase.js';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 export const DetailsPage = ({ navigation, route }) => {
   const message = route.params?.message;
   const [text, setText] = useState(message.content);
   const [imagePath, setImagePath] = useState(null);
-  function saveAndGoToHome() {
-
-    navigation.navigate("Home", { content: text, key: message.key })
-  }
-
-  async function updateNote(item) {
-    try {
-      await updateDoc(doc(database, "notes", item.id), { content: item.content });
-    } catch (error) {
-      console.log("Error updating note: ", error);
-    }
-  }
-  async function launchImagePicker() {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-    })
-    if (!result.cancelled) {
-      setImagePath(result.assets[0].uri);
-    }
-  }
-  async function uploadImage() {
+  
+  async function saveAndGoToHome() {
+    if (imagePath) {
+      try {
     const res = await fetch(imagePath);
     const blob = await res.blob();
 
-    const uniqueFileName = `image_${new Date().getTime()}.jpg}`
+    const uniqueFileName = `image_${new Date().getTime()}.jpg`
 
     const storageRef = ref(storage, uniqueFileName);
     await uploadBytes(storageRef, blob).then((snapshot) => {
       console.log('Uploaded a blob!');
     });
+    } catch(error) {
+      console.log("Error uploading image: ", error);
+    
+    }
+  }
+    try{
+      const noteRef = doc(database, "notes", message.id);
+      await updateDoc(noteRef, { content: text });
+      console.log("Document updated in Forestorewith ID: ", message.id);
+    } catch(error) {
+      console.log("Error updating note: ", error);
+    }
+
+    navigation.navigate("Home")
+  }
+  
+  // not used in this version
+  async function updateNote(item) {
+    try{
+      const noteRef = doc(database, "notes", message.id);
+      await updateDoc(noteRef, { content: text });
+      console.log("Document updated in Forestorewith ID: ", message.id);
+    } catch(error) {
+      console.log("Error updating note: ", error);
+    }
+  }
+
+  async function uploadImage() {
+    const res = await fetch(imagePath);
+    const blob = await res.blob();
+
+    const uniqueFileName = `image_${new Date().getTime()}.jpg`
+
+    const storageRef = ref(storage, uniqueFileName);
+    await uploadBytes(storageRef, blob).then((snapshot) => {
+      console.log('Uploaded a blob!');
+    });
+  }
+
+  async function launchImagePicker() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    });
+    if (!result.cancelled) {
+      const uri = result.assets ? result.assets[0].uri : result.uri;
+      setImagePath(uri);
+    }
+  }
+
+  async function takePhoto() {
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    if (permissionResult.granted === false) {
+      Alert.alert("You've refused to allow this app to access your camera!");
+      return;
+    }
+
+    let result = await ImagePicker.launchCameraAsync();
+
+    console.log(result); 
+
+    if (!result.cancelled) {
+      const uri = result.assets ? result.assets[0].uri : result.uri;
+      console.log(uri);
+      setImagePath(uri);
+    } else {
+      console.log('Image picker was cancelled or failed');
+    }
   }
   async function downloadImage() {
     const storageRef = ref(storage, "myimage.jpg");
@@ -63,6 +114,7 @@ export const DetailsPage = ({ navigation, route }) => {
         <Image style={styles.image} source={{ uri: imagePath }} />
         <Button style={styles.downloadButton} title="Download Image" onPress={downloadImage} />
         <Button style={styles.saveButton} title="Add Image" onPress={launchImagePicker} />
+        <Button style={styles.saveButton} title="Take Photo" onPress={takePhoto} />
         <Button style={styles.uploadButton} title="Upload Image" onPress={uploadImage} />
       </View>
     </View>
